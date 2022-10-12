563a494a2f834c920b663ed77bf484ca0b0a3fad
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-10-12 17:08:38 -0400

    Update Remove-Dups-Arr.java

--- Easy/Lists-ND-Arrays/Remove-Dups-Arr.java
+++ Easy/Lists-ND-Arrays/Remove-Dups-Arr.java
@@ -4,11 +4,11 @@ class Solution {
         // Loop for all the elements in the array
         for (int i = 0; i < nums.length; i++) {
             // If the current element is equal to the next element, we skip
-            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) { //if not last element and is duplicate skip to next iteration
                 continue;
             }
             // We will update the array in place
-            nums[count] = nums[i];
+            nums[count] = nums[i]; //count only updates when duplicate not found and not at end of array, count will "move" list element by element
             count++;
         }
         return count;
